73aa3124f819e2b5c38f2aa0f09379d62a1bab7d
Konrad Przewłoka <konrad.przewloka@gmail.com>
2018-08-08 18:42:25 +0200

    fix:Fixed problems form review

--- sw.js
+++ sw.js
@@ -31,7 +31,10 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
+      if (response) {
+           return response;
+         }
+         return fetch(event.request);
     })
   );
 });
